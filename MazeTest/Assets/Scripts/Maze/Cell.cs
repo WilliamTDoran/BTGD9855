6681e1c04c90d7332ffb5557ef6d2a421befd20e
acapple <acapple2002@gmail.com>
2022-10-12 12:40:03 -0400

    Added my maze algorithm
    Also attempted 2 biomes

---
+++ MazeTest/Assets/Scripts/Maze/Cell.cs
@@ -0,0 +1,121 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    //The 4 walls of the cell
+    [SerializeField]
+    [Tooltip("North, East, South, West order please.")]
+    private Wall[] walls;
+
+    private Maze m;
+
+    //int i = 0;
+
+    private bool inMaze = false;
+
+    [SerializeField]
+    private GameObject floor;
+
+    void Awake()
+    {
+        m = GameObject.FindGameObjectsWithTag("Maze")[0].GetComponent<Maze>();
+    }
+
+    //hey, what wall you need?
+    public Wall getWall(int w)
+    {
+        if (w >= 0 && w < walls.Length) 
+        {
+            return walls[w];
+        } else
+        {
+            Debug.Log("Index doesn't exist on cell " + gameObject.name);
+            return null;
+        }
+    }
+
+    public bool isInMaze()
+    {
+        return inMaze;
+    }
+
+    //helper function to help setup the maze. Connects this cell to the cell on the left
+    public void leftCell(Cell l)
+    {
+        if (l != null)
+        {
+            connect(walls[(int)Wall.wLocation.west], l.getWall((int)Wall.wLocation.east));
+        }
+    }
+
+    //helper function help setup the maze. Connects this cell to the cell above
+    public void topCell(Cell t)
+    {
+        if (t != null)
+        {
+            connect(walls[(int)Wall.wLocation.north], t.getWall((int)Wall.wLocation.south));
+        }
+    }
+
+    //helper function. links two walls
+    private void connect (Wall here, Wall there)
+    {
+        //Debug.Log("Connecting cells." + here.name + " and " + there.name);
+        here.linkWall(there);
+        there.linkWall(here);
+    }
+
+
+    //The main loop of the maze Algorithm
+    //Note to self: later may need to make into coroutine
+    public void generateMaze(Color c, Maze.runningGenerator thisGen)
+    {
+        inMaze = true;
+        floor.GetComponent<Renderer>().material.color = c;
+        //swap function running
+        int temp = (int) (m.running + 1) % (int)Maze.runningGenerator.length;
+        m.running = (Maze.runningGenerator)temp;
+        Debug.Log(m.running + " from " + c+", pre loop");
+        temp = 0;
+        while (m.running != thisGen)
+        {
+            temp++;
+            Debug.Log(m.running + " from " + c + ", loop iteration " + temp);
+            if (temp >= 1000) 
+            {
+                break;
+            }
+        }
+        //Debug.Log("Cell " + gameObject.name + " is being connected to the maze");
+        //pick a cell around it.
+        Cell nextCell;
+        int dir;
+        for (int j = 0; j < 4; j++) 
+        {
+            dir = (int)Random.Range(0, 4);
+            nextCell = getWall(dir).getLink().getCell(); //Rigged to return itself if not possible
+            //Make sure it's in the maze. If not select a new cell
+            for (int i = 0; i < 4; i++)
+            {
+                if (!nextCell.isInMaze())
+                {
+                    break;
+                }
+                dir++;
+                dir %= 4;
+                nextCell = getWall(dir).getLink().getCell();
+            }
+            //if all are, this is a deadend. go back up the generate maze loop
+            if (nextCell.isInMaze())
+            {
+                return;
+            }
+            //Connect the cell that's not connected
+            walls[dir].hit(false);
+            nextCell.generateMaze(c, thisGen);
+        }
+        return;
+    }
+}
---
+++ MazeTest/Assets/Scripts/Maze/Maze.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maze : MonoBehaviour
+{
+    //Maze modifiers
+    [SerializeField]
+    int width;
+    [SerializeField]
+    int height;
+    [SerializeField]
+    float scale = 1;
+
+    //Just to store the rows
+    [SerializeField]
+    GameObject sampleRow;
+
+    private IEnumerator generation1;
+
+
+    public enum runningGenerator
+    {
+        Green,
+        Red,
+        length
+    }
+
+    public runningGenerator running = runningGenerator.Green;
+
+    //stores a a bunch of rows of cells
+    Row[] rows;
+
+    //Hey! We need a maze here.
+    void Start()
+    {
+        //GameObject.Find("Floor").transform.localScale = new Vector3(scale*5*width,height*scale*5, 1);
+        initMaze();
+    }
+
+    //Creates a grid of cells
+    void initMaze()
+    {
+        //scales up the maze to scale size for easy conversion
+        transform.localScale = new Vector3(scale, scale, 1);
+        //create a bunch of rows
+        rows = new Row[height];
+        for (int i = 0; i < rows.Length; i++) 
+        {
+            GameObject temp = Instantiate(sampleRow, new Vector3(0, -1*i*scale, 0), Quaternion.identity, transform);
+            temp.name = "Row " + i;
+            rows[i] = temp.GetComponent<Row>();
+            if (i > 0) 
+            {
+                rows[i].initRow(width, i, scale, rows[i-1]);
+            } else
+            {
+                rows[i].initRow(width, i, scale);
+            }
+        }
+
+        generateMaze();
+    }
+
+    public void generateMaze()
+    {
+        generation1 = coRunning();
+        StartCoroutine(generation1);
+        getCell((int)width / 2 - 1, (int)height / 2).generateMaze(new Color(1, 0, 0, 1), runningGenerator.Red);
+    }
+
+    private IEnumerator coRunning()
+    {
+        getCell((int)width / 2 + 1, (int)height / 2).generateMaze(new Color(0, 1, 0, 1), runningGenerator.Green);
+        yield return null;
+    }
+
+    //helper function, return cell (x,y) from the maze.
+    public Cell getCell(int x, int y)
+    {
+        return rows[y].getCell(x);
+    }
+}
