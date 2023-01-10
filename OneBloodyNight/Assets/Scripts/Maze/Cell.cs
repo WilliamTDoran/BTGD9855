8264249af5f6cacca6abf5ae664602a6899e1e41
acapple <acapple2002@gmail.com>
2023-01-10 14:00:40 -0500

    Maze merged into main scene

---
+++ OneBloodyNight/Assets/Scripts/Maze/Cell.cs
@@ -0,0 +1,118 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour
+{
+    //The 4 walls of the cell
+    [SerializeField]
+    [Tooltip("North, East, South, West order please.")]
+    private Wall[] walls;
+
+    [SerializeField]
+    private BiomeGenerator.Biome biome = BiomeGenerator.Biome.length;
+
+    private bool inMaze = false;
+
+    [SerializeField]
+    private GameObject floor;
+
+    void Awake()
+    {
+    }
+
+    //hey, what wall you need?
+    public Wall getWall(int w)
+    {
+        if (w >= 0 && w < walls.Length) 
+        {
+            return walls[w];
+        } else
+        {
+            Debug.Log("Index doesn't exist on cell " + gameObject.name);
+            return null;
+        }
+    }
+
+    public BiomeGenerator.Biome getBiome()
+    {
+        return biome;
+    }
+
+    public void setBiome(BiomeGenerator.Biome b)
+    {
+        biome = b;
+    }
+
+    void Update()
+    {
+        floor.GetComponent<Renderer>().material.color = Maze.m.biomeVariables[(int)biome].colour;
+    }
+
+    public bool isInMaze()
+    {
+        return inMaze;
+    }
+
+    //helper function to help setup the maze. Connects this cell to the cell on the left
+    public void leftCell(Cell l)
+    {
+        if (l != null)
+        {
+            connect(walls[(int)Wall.wLocation.west], l.getWall((int)Wall.wLocation.east));
+        }
+    }
+
+    //helper function help setup the maze. Connects this cell to the cell above
+    public void topCell(Cell t)
+    {
+        if (t != null)
+        {
+            connect(walls[(int)Wall.wLocation.north], t.getWall((int)Wall.wLocation.south));
+        }
+    }
+
+    //helper function. links two walls
+    private void connect (Wall here, Wall there)
+    {
+        //Debug.Log("Connecting cells." + here.name + " and " + there.name);
+        here.linkWall(there);
+        there.linkWall(here);
+    }
+
+
+    //The main loop of the maze Algorithm
+    //Note to self: later may need to make into coroutine
+    public void generateMaze()
+    {
+        inMaze = true;
+        //pick a cell around it.
+        Cell nextCell;
+        int dir;
+        for (int j = 0; j < 4; j++) 
+        {
+            dir = (int) Random.Range(0, 4);
+            nextCell = getWall(dir).getLink().getCell(); //Rigged to return itself if not possible
+            //Make sure it's in the maze. If not select a new cell
+            for (int i = 0; i < 4; i++)
+            {
+                if (!nextCell.isInMaze() && nextCell.getBiome() == biome)
+                {
+                    break;
+                }
+                dir++;
+                dir %= 4;
+                nextCell = getWall(dir).getLink().getCell();
+            }
+            //if all are, this is a deadend. go back up the generate maze loop
+            if (nextCell.isInMaze() || nextCell.getBiome() != biome)
+            {
+                return;
+            }
+            //Connect the cell that's not connected
+            walls[dir].hit(false);
+            nextCell.generateMaze();
+        }
+        return;
+    }
+}
---
+++ OneBloodyNight/Assets/Scripts/Maze/Row.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Row : MonoBehaviour
+{
+    //Info about this rows
+    private int width;
+    private int y; //how low down the maze this row is. Top=0, bottom=Maze.height;
+    private float scale;
+    private Cell[] cells;
+
+    //What is created
+    [SerializeField]
+    private GameObject sampleCell;
+
+    //heper function. Gets a cell from a row
+    public Cell getCell(int x)
+    {
+        return cells[x];
+    }
+
+    //initiate the top row. (For no array out of bounds)
+    public void initRow(int w, int h, float s)
+    {
+        initRow(w, h, s, null);
+    }
+
+    //initiate the row. (for grid of cells)
+    public void initRow(int w, int h, float s, Row above)
+    {
+        //assign variable values
+        y = h;
+        width = w;
+        scale = s;
+        //initiate array of cells
+        cells = new Cell[width];
+        for (int i = 0; i < cells.Length; i++) 
+        {
+            GameObject temp = Instantiate(sampleCell, new Vector3(i*scale, transform.position.y, 0), Quaternion.identity, transform);
+            temp.name = "Cell (" + i + "," + y + ")";
+            cells[i] = temp.GetComponent<Cell>();
+            if (i > 0) 
+            {
+                cells[i].leftCell(cells[i - 1]);
+            }
+            if (y > 0)
+            {
+                cells[i].topCell(above.getCell(i));
+            }
+        }
+    }
+}
