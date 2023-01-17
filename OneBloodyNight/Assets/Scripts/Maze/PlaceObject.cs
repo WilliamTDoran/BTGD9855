b41264a8dd138b9bcb8b7c113f713e258a534235
acapple <acapple2002@gmail.com>
2023-01-17 11:20:45 -0500

    Added object forced spawning

--- OneBloodyNight/Assets/Scripts/Maze/PlaceObject.cs
+++ OneBloodyNight/Assets/Scripts/Maze/PlaceObject.cs
@@ -1,28 +1,28 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class PlaceObject
 {
-    public void massPlace(Biome b, PlacableObject obj)
+    public static void massPlace(Biome b, PlacableObject obj)
     {
-        for (int i = Random.Range(obj.minPlaced, obj.maxPlaced); i > 0; i--) 
-        {
-            place(b, obj);
-        }
+        int max = (int)Math.Floor(Random.Range(obj.minPlaced, obj.maxPlaced));
     }
 
-    public void place(Biome b, PlacableObject obj)
+    public static void place(Biome b, PlacableObject obj)
     {
         int x;
         int y;
-        cell(b, obj.maxPerCell, obj.cellDifficulty, out x, out y);
-        //GameObject placed = GameObject.Instantiate(obj.placing, new Vector3(Maze.m.getWorldPosX(x), Maze.m.getWorldPosX(x), obj.placing.transform.position.z), new Quaternion());
-        Cell c = Maze.m.getCell(x, y);
-        //placed.transform.parent = c.spawner.transform;
+        cell(b, obj.cellDifficulty, out x, out y);
+        GameObject placed = GameObject.Instantiate(obj.formation, new Vector3(Maze.m.getWorldPosX(x), -Maze.m.getWorldPosX(y), obj.formation.transform.position.z), new Quaternion());
+        //Debug.Log("Object placed in cell "+x+", "+y+", biome colour: "+Maze.m.getCell(x,y).getBiome());
+        //Maze.m.getCell(x, y).transform.GetChild(0).gameObject.SetActive(true);
+        Maze.m.getCell(x, y).transform.GetChild(0).GetComponent<Spawner>().addSpawnLocations(placed);
     }
 
-    public void cell(Biome b, short maxPerCell, float diff, out int x, out int y)
+    public static void cell(Biome b, float diff, out int x, out int y)
     {
         Cell c;
         do
@@ -30,7 +30,7 @@ public class PlaceObject
             x = Random.Range(0, Maze.m.width());
             y = Random.Range(0, Maze.m.height());
             c = Maze.m.getCell(x, y);
-            // while the biomes don't match & the cell's difficulty is too high & the
-        } while (c.getBiome() != b && diff+c.difficulty >= Maze.m.biomeVariables[(int)c.getBiome()].maxCellDifficulty && c.placed > maxPerCell);
+            // while the biomes don't match & the cell's difficulty is too high
+        } while (c.getBiome() != b && diff+c.difficulty >= Maze.m.biomeVariables[(int)b].maxCellDifficulty);
     }
 }
--- OneBloodyNight/Assets/Scripts/Maze/Spawner.cs
+++ OneBloodyNight/Assets/Scripts/Maze/Spawner.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    GameObject[] spawns;
+    Spawn[] spawns;
+
+    private void Awake()
+    {
+        //gameObject.SetActive(false);
+    }
 
     private void Start()
     {
@@ -19,19 +24,62 @@ public class Spawner : MonoBehaviour
         }
     }
 
+    internal void addSpawnLocations(GameObject obj)
+    {
+        if (spawns == null)
+        {
+            spawns = new Spawn[0];
+        }
+        Debug.Log("Adding Object");
+        for(int i=0; i < obj.transform.childCount; i++)
+        {
+            Debug.Log("Pass "+i);
+            Transform c = obj.transform.GetChild(i);
+            Spawn[] s = new Spawn[spawns.Length + 1];
+            for (int j = 0; j<spawns.Length; j++)
+            {
+                s[j] = spawns[j];
+            }
+            s[spawns.Length] = new Spawn((int)c.position.x, (int)c.position.y, c.gameObject);
+            spawns = s;
+        }
+    }
+
     private void OnTriggerEnter(Collider other)
     {
+        //Debug.Log("Collision enter! " + other.gameObject.name);
         if (other.tag == "Loaded")
         {
+            //Debug.Log("in");
+            if (spawns == null)
+            {
+                return;
+            }
             //Load spawns in
+            foreach (Spawn s in spawns)
+            {
+                s.obj.transform.position = new Vector3(s.x, s.y, s.obj.transform.position.z);
+                s.obj.SetActive(true);
+            }
         }
     }
 
     private void OnTriggerExit(Collider other)
     {
+        //Debug.Log("Collision exit! " + other.gameObject.name);
         if (other.tag == "Loaded")
         {
+            //Debug.Log("in");
+            if (spawns == null)
+            {
+                return;
+            }
             //unload spawns in
+            foreach (Spawn s in spawns)
+            {
+                //s.obj.transform.position = new Vector3(s.x, s.y, s.obj.transform.position.z);
+                s.obj.SetActive(false);
+            }
         }
     }
 }
