a1d3781fd2a227ff8192911bfa34bf1a134bc0f2
TariaTuuri <tt20ll@brocku.ca>
2023-01-22 01:05:14 -0500

    Bloodmeter First pass

---
+++ OneBloodyNight/Assets/Scripts/Bloodmeter.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Bloodmeter : MonoBehaviour
+{
+    public Slider bloodmeter;
+    private int currentBlood;
+    // Start is called before the first frame update
+    void Start()
+    {
+        bloodmeter.value = bloodmeter.maxValue;
+        Debug.Log("Done");
+        StartCoroutine("DMG");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator DMG()
+    {
+
+        while (bloodmeter.value > bloodmeter.minValue)
+        {
+            bloodmeter.value = bloodmeter.value - 1;//takes 1 blood per second
+            yield return new WaitForSeconds(0.25f);//slows down the damage rate
+        }
+        yield return null;//Player is dead
+    }
+}
