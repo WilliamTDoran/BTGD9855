01ab5db8e16811b6bda11b2653b9a76ef4c802a9
WilliamTDoran <71032493+WilliamTDoran@users.noreply.github.com>
2023-01-22 17:00:55 -0500

    Shader stuff on player, plus beginning of CombatManager

--- OneBloodyNight/Assets/Scripts/CombatManager.cs
+++ OneBloodyNight/Assets/Scripts/CombatManager.cs
@@ -4,15 +4,64 @@ using UnityEngine;
 
 public class CombatManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private IEnumerator immuneCoroutine;
+
+    private static CombatManager instance;
+    public static CombatManager Instance { get { return instance; } }
+
+    private void Awake()
     {
-        
+        if (instance != null & instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    public void HarmTarget(GameActor attacker, GameActor target, int amount, float knockBackAmount)
     {
-        
+        if (!target.Immune)
+        {
+            //target.HitPoints -= amount;
+            Debug.Log("Send Damage to " + target.gameObject.name + " from " + attacker.gameObject.name);
+            ApplyKnockback(attacker, target, knockBackAmount);
+        }
+    }
+
+    private void ApplyKnockback(GameActor attacker, GameActor target, float amount)
+    {
+        Rigidbody attackerRigidBody = attacker.Rb;
+        Rigidbody targetRigidBody = target.Rb;
+
+        Vector3 direction = targetRigidBody.position - attackerRigidBody.position;
+        direction.Normalize();
+
+        direction *= amount;
+
+        targetRigidBody.velocity += direction;
+    }
+
+    private IEnumerator ImmuneCountdown(GameActor target, float immuneDuration)
+    {
+        target.Immune = true;
+
+        yield return new WaitForSeconds(immuneDuration);
+
+        target.Immune = false;
+    }
+
+    private void StartImmuneCountdown(GameActor target, float immuneDuration)
+    {
+        immuneCoroutine = ImmuneCountdown(target, immuneDuration);
+        StartCoroutine(immuneCoroutine);
+    }
+
+    private void StopImmuneCountdown()
+    {
+        StopCoroutine(immuneCoroutine);
+        immuneCoroutine = null;
     }
 }
