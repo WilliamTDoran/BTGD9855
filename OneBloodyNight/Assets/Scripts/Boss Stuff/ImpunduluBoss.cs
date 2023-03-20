a3bbeea5de29e8f969458ea0873bf882690e053d
WilliamTDoran <71032493+WilliamTDoran@users.noreply.github.com>
2023-03-20 17:57:31 -0400

    Groundwork for phases

--- OneBloodyNight/Assets/Scripts/Boss Stuff/ImpunduluBoss.cs
+++ OneBloodyNight/Assets/Scripts/Boss Stuff/ImpunduluBoss.cs
@@ -15,43 +15,73 @@ public class ImpunduluBoss : Boss
     public AudioClip Intro;
     /*~~~~~~~*/
 
+    private System.Random rnd;
+    private int rndCap = 3;
+
     private IEnumerator spinAttackCoroutine;
     private IEnumerator diveAttackCoroutine;
     private IEnumerator randomBehaviorCoroutine;
     private IEnumerator randomAttackingCoroutine;
+    private IEnumerator phaseCheckCoroutine;
 
     private Vector3 faceDirection;
 
     private int lightningCycle = 0;
 
+    [Tooltip("Reference to all feather objects")]
     [SerializeField]
     private Projectile[] feathers;
 
+    [Tooltip("Reference to all lightning objects")]
     [SerializeField]
     private RemoteAttack[] lightnings;
 
+    [Tooltip("Time between individual feathers")]
     [SerializeField]
     private float timeBetweenFeathers = 0.6f;
 
+    [Tooltip("Reference to the main camera, used for swoop positioning")]
     [SerializeField]
     private Camera cam;
 
+    [Header("Attack Timings")]
+    [SerializeField]
+    private float timeBeforeFeathers;
+
+    [SerializeField]
+    private float timeBeforeLightning;
+
+    [SerializeField]
+    private float timeBeforeSwoop;
+
+    [SerializeField]
+    private float timeBeforeHoming;
+
+    [SerializeField]
+    private float timeBeforeBeam;
+
     [Header("Dive")]
+    [Tooltip("The hitbox/attack for the swoop")]
     [SerializeField]
     private Attack swoopBox;
 
+    [Tooltip("How long it takes the 'dulu to reach the side of the screen pre-swoop, in seconds")]
     [SerializeField]
     private float initialPositionTime = 1.0f;
 
+    [Tooltip("How long the 'dulu holds to the side of the screen pre-swoop, in seconds")]
     [SerializeField]
     private float threatenTime = 0.75f;
 
+    [Tooltip("How fast the 'dulu swoops")]
     [SerializeField]
     private float diveSpeed = 3000;
 
+    [Tooltip("How long the 'dulu stands still after finishing the swoop, in seconds")]
     [SerializeField]
     private float stopTime = 0.4f;
 
+    [Tooltip("How long the 'dulu takes to return to its starting position, in seconds")]
     [SerializeField]
     private float recoverTime = 0.3f;
 
@@ -59,6 +89,9 @@ public class ImpunduluBoss : Boss
 
     protected override void Start()
     {
+        rnd = new System.Random();
+        StartCoroutine(PhaseCheck());
+
         //audioSource.PlayOneShot(Intro,2f);
         base.Start();
         animator.SetTrigger("start");
@@ -212,18 +245,21 @@ public class ImpunduluBoss : Boss
 
     private IEnumerator RandomAttacking()
     {
-        yield return new WaitForSeconds(UnityEngine.Random.Range(1.5f, 4.0f));
-
-        int check = (int)UnityEngine.Random.Range(0, 2.99999999f);
+        int check = rnd.Next(0, rndCap + currentPhase);
 
         switch (check)
         {
             case 0:
+                yield return new WaitForSeconds(timeBeforeFeathers);
+
                 StartSpinAttack();
                 animator.SetTrigger("spin");
                 
                 break;
+
             case 1:
+                yield return new WaitForSeconds(timeBeforeLightning);
+
                 lightnings[lightningCycle].Initiate(Player.plr.Rb.position);
 
                 lightningCycle = lightningCycle > 1 ? 0 : lightningCycle + 1;
@@ -232,13 +268,43 @@ public class ImpunduluBoss : Boss
                 StopRandomBehavior();
                 StartRandomBehavior();
                 break;
+
             case 2:
+                yield return new WaitForSeconds(timeBeforeSwoop);
+
                 animator.ResetTrigger("spin");
                 StartDiveAttack();
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                break;
+
+            default:
+                yield return new WaitForSeconds(timeBeforeFeathers);
+
+                StartSpinAttack();
+                animator.SetTrigger("spin");
+
                 break;
         }
     }
 
+    private IEnumerator PhaseCheck()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(0.5f);
+
+            float healthPercent = (float)CurHitPoints / (float)MaxHitPoints;
+            if (healthPercent <= phases[currentPhase + 1])
+            {
+                currentPhase++;
+            }
+        }
+    }
 
 
     private void StartSpinAttack()
@@ -291,5 +357,17 @@ public class ImpunduluBoss : Boss
     {
         StopCoroutine(randomAttackingCoroutine);
         randomAttackingCoroutine = null;
+    }
+
+    private void StartPhaseCheck()
+    {
+        phaseCheckCoroutine = PhaseCheck();
+        StartCoroutine(phaseCheckCoroutine);
+    }
+
+    private void StopPhaseCheck()
+    {
+        StopCoroutine(phaseCheckCoroutine);
+        phaseCheckCoroutine = null;
     }
 }
