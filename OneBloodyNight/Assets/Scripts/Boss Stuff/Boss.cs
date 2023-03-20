9d0fef5c3040267d8bdeddaa688bdbccafa37a92
WilliamTDoran <71032493+WilliamTDoran@users.noreply.github.com>
2023-03-20 16:02:37 -0400

    Feather positional stuff

--- OneBloodyNight/Assets/Scripts/Boss Stuff/Boss.cs
+++ OneBloodyNight/Assets/Scripts/Boss Stuff/Boss.cs
@@ -4,17 +4,15 @@ using UnityEngine;
 
 public class Boss : GameActor
 {
-    private Phase activePhase;
-
     /* Exposed Variables */
     [Header("Boss")]
-    [Tooltip("The number of stages, in order from first to last, with the hp percent threshold for when the stage ENDS")]
+    [Tooltip("The number of phases. The value of each should be the health percent when the boss will ENTER the phase")]
     [SerializeField]
-    private Phase[] phases;
+    protected float[] phases;
 
     [Tooltip("Current phase (Only change manually for testing, always change back afterward)")]
     [SerializeField]
-    private int currentPhase = 0;
+    protected int currentPhase = 0;
     /*~~~~~~~~~~~~~~~~~~~*/
 
     protected override void Awake()
@@ -38,7 +36,6 @@ public class Boss : GameActor
 
     internal void Transition(int targetPhase)
     {
-        activePhase = phases[targetPhase];
         currentPhase = targetPhase;
     }
 }
