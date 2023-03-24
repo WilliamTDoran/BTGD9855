2060fb1178bf4b57c67e2f0b9e2c03ecf4f0be92
TariaTuuri <tt20ll@brocku.ca>
2023-03-24 08:49:27 -0400

    Ability Icons in

---
+++ OneBloodyNight/Assets/Scripts/UI/AbilityHUD.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbilityHUD : MonoBehaviour
+{
+    public GameObject Empty;
+    public GameObject batSwarm;
+    public GameObject Invis;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Empty.SetActive(true);
+        batSwarm.SetActive(false);
+        Invis.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Invis.SetActive(false);
+            batSwarm.SetActive(true);
+        }
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            batSwarm.SetActive(false);
+            Invis.SetActive(true);
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            Empty.SetActive(true);
+            Invis.SetActive(false);
+            batSwarm.SetActive(false);
+        }
+    }
+}
