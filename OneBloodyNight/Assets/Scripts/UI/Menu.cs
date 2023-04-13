953ae3f065a90b453363525f77d5844ec6fab511
TariaTuuri <tt20ll@brocku.ca>
2023-04-12 22:13:28 -0400

    misc

--- OneBloodyNight/Assets/Scripts/UI/Menu.cs
+++ OneBloodyNight/Assets/Scripts/UI/Menu.cs
@@ -35,6 +35,7 @@ public class Menu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        PlayerPrefs.DeleteAll();
         Background.SetActive(true);
         optionScreen.SetActive(false);
         loreScreen.SetActive(false);
